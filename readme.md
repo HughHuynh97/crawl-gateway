fdf5908b19151f0d5f943c08fb5ff8cde6a1f442
trivip002 <40769628+trivip002@users.noreply.github.com>
2022-06-04 15:16:03 +0700

    # Init

---
+++ readme.md
@@ -0,0 +1,3 @@
+- Build Docker: mvn clean package -DskipTests && docker build -t spring-boot-docker-compose .
+- Run Compose: Docker compose up
+- Stop Compose: Docker compose down
--- src/main/java/com/product/controller/InternalController.java
+++ src/main/java/com/product/controller/InternalController.java
@@ -1,7 +1,25 @@
 package com.product.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+@RequestMapping("internal/api")
 public class InternalController {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @GetMapping("get-resource-info")
+    public Object getResoruce() {
+        return Map.of("url", url, "username", username, "password", password);
+    }
 }

51ac04467b42705738dd11005afe37ae66db1ba1
trivip002 <40769628+trivip002@users.noreply.github.com>
2022-06-10 20:19:16 +0700

    # Test Sonarlint

--- src/main/java/com/crawl/model/CrawlQueue.java
+++ src/main/java/com/crawl/model/CrawlQueue.java
@@ -18,9 +18,6 @@ public class CrawlQueue {
         this.startDate = rs.getString("start_date");
         this.endDate = rs.getString("end_date");
         this.cause = rs.getString("cause");
-        this.cause = rs.getString("cause");
-        this.cause = rs.getString("cause");
-        this.cause = rs.getString("cause");
         this.status = rs.getString("status");
     }
 }

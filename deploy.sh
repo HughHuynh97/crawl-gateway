5d378646eb0516dba8abc19f24a6b0a609247db9
trivip002 <40769628+trivip002@users.noreply.github.com>
2022-06-11 01:00:04 +0700

    # Init

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 app="crawl-service"
 username="trivip002"
-echo Build Image
+echo Pull Image
 docker stop "$app" && docker rm -f "$app"
 docker pull "$username"/"$app":latest
+echo Create Container
 docker create -p 8080:8080 --name "$app" --net spring-net -e MYSQL_HOST=mysqldb -e MYSQL_USERNAME=root -e MYSQL_PASSWORD=tripro123 -e MYSQL_PORT=3306 -e MYSQL_DATABASE=dev "$username"/"$app"

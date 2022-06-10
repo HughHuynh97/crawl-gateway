7e52a3e5223b357b20e51d78f9bc03013c53cc38
trivip002 <40769628+trivip002@users.noreply.github.com>
2022-06-10 19:29:43 +0700

    # Test Sonarlint

--- src/main/java/com/crawl/model/CrawlQueue.java
+++ src/main/java/com/crawl/model/CrawlQueue.java
@@ -16,6 +16,8 @@ public class CrawlQueue {
     public CrawlQueue(ResultSet rs) throws SQLException {
         this.queueId = rs.getLong("queue_id");
         this.startDate = rs.getString("start_date");
+        this.startDate = rs.getString("start_date");
+        this.startDate = rs.getString("start_date");
         this.endDate = rs.getString("end_date");
         this.cause = rs.getString("cause");
         this.status = rs.getString("status");

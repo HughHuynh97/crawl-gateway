f9335bfe8e47ac8f84b70a0ffa3a8936e5fe1759
trivip002 <40769628+trivip002@users.noreply.github.com>
2022-06-11 00:57:41 +0700

    # Init

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ username="trivip002"
 echo Build Image
 docker stop "$app" && docker rm -f "$app"
 docker pull "$username"/"$app":latest
-docker run -p 8080:8080 --name "$app" --net spring-net -e MYSQL_HOST=mysqldb -e MYSQL_USERNAME=root -e MYSQL_PASSWORD=tripro123 -e MYSQL_PORT=3306 -e MYSQL_DATABASE=dev "$username"/"$app"
+docker create -p 8080:8080 --name "$app" --net spring-net -e MYSQL_HOST=mysqldb -e MYSQL_USERNAME=root -e MYSQL_PASSWORD=tripro123 -e MYSQL_PORT=3306 -e MYSQL_DATABASE=dev "$username"/"$app"
